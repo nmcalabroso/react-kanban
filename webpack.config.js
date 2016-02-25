4353790056bac77daf882108fe3a5ae3d24821b5
Neil Calabroso <ncalabroso@gmail.com>
2016-02-26 03:08:32 +0800

    Initial Commit
    Bookmark: http://survivejs.com/webpack_react/webpack_and_react/#developing-the-first-react-view

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const npmInstall = require('npm-install-webpack-plugin');
+
+const TARGET = process.env.npm_lifecycle_event;
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build'),
+  eslint: path.join(__dirname, '.eslintrc'),
+  stylelint: path.join(__dirname, '.stylelint.config.js')
+};
+
+const common = {
+  entry: {
+    app: PATHS.app
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'eslint-loader',
+        include: PATHS.app
+      },
+      {
+        test: /\.scss$/,
+        loader: 'stylelint',
+        include: PATHS.app
+      }
+    ],
+    loaders: [
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass'],
+        include: PATHS.app
+      },
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel?cacheDirectory'],
+        include: PATHS.app
+      },
+    ]
+  },
+  stylelint: {
+    configFile: PATHS.stylelint
+  },
+  eslint: {
+    configFile: PATHS.eslint
+  },
+};
+
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      contentBase: PATHS.build,
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      stats: 'errors-only',
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new npmInstall({
+        save: true
+      })
+    ]
+  });
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
+}
